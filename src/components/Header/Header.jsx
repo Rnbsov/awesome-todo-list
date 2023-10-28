42048c379547f19d56e55d09330b14706e318ade
Rnbsov <rnbsov@gmail.com>
2023-10-28 23:49:57 +0600

    ✨ Add task button improvement

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,21 +8,21 @@ const Header = () => {
     >
       <p>Title</p>
       {/* <AddButton /> */}
-      <div className='flex items-center justify-center gap-5'>
+      <div className='flex items-center justify-center gap-x-4 lg:gap-x-6'>
         <div className='relative flex items-center justify-center'>
           <input
             type='search'
             placeholder='Search'
             name='search'
-            className='rounded-2xl outline-none pl-10 bg-transparent border-[3px] border-darkOutline px-5 py-2   focus:border-purple-600 '
+            className='rounded-2xl outline-none pl-10 bg-transparent border-[3px] border-darkOutline px-5 py-2   focus:border-purple-600'
           />
           <Search color='#8F8D9A' className='absolute left-2' />
         </div>
 
-        <button className='w-6 h-6 bg-gradient-to-tr from-cl1 to-cl2 flex justify-center items-center rounded text-white p-1'>
+        <button className='w-8 h-8 bg-gradient-to-tr from-cl1 to-cl2 flex justify-center items-center rounded-lg text-white transition-transform duration-300 ease-in-out hover:scale-110'>
           <Plus size={24} />
         </button>
-
+        
         <img
           className='w-10 h-10 rounded-full'
           src='https://rnbsov.gallerycdn.vsassets.io/extensions/rnbsov/github-purple-refined/1.2.4/1696257237537/Microsoft.VisualStudio.Services.Icons.Default'
